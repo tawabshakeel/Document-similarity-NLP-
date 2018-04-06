8f0fe56ed0f1af224e89aa1c1ee81faa27d92b3e
tawab <tawab2013@gmail.com>
2018-04-06 11:23:20 +0500

    updates in task 1 and completion of task 2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from collections import Counter;
+import nltk as nt
 
-def countingAllWords():
+
+def countingAllWords(str):
     counts = dict()
-    for line in open ('books/4300-0.txt', 'r', encoding="utf8"):
-      for word in line.split ():
+    for line in open ('books/'+str, 'r', encoding="utf8"):
+      for word in line.split():
           word = word.lower().replace('.','').replace('?','').replace('!','').replace(':','').replace(',','').replace(')','').replace('(','')
           if word in counts:
             counts[word] += 1
@@ -12,18 +13,37 @@ def countingAllWords():
 
     return counts
 
-
-def checking(str):
-    counts = dict()
-    for word in str.split():
-        word=word.lower().replace('.','')
-        if word in counts:
-            print("already present")
-            counts[word] += 1
-        else:
-            print("not present")
-            counts[word] = 1
-    return counts
+# print(countingAllWords('4300-0.txt'))
 
 
-print(countingAllWords())
+
+def separating_nouns_and_verbs(book) :
+    File = open('books/'+book,encoding='utf8')  # open file
+    lines = File.read()  # read all lines
+    sentences = nt.sent_tokenize(lines)  # tokenize sentences
+    nouns = dict()  # empty to array to hold all nouns
+    verbs= dict()
+
+    for sentence in sentences:
+        for word, pos in nt.pos_tag(nt.word_tokenize(str(sentence))):
+            word = word.lower()
+            if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
+                if word in  nouns:
+                    nouns[word] += 1
+                    print("containing")
+                else:
+                    nouns[word] = 1
+                    print(" not containing")
+            elif (pos == 'VBP' or pos == 'VB' or pos == 'VBZ' ):
+                if word in  verbs:
+                    verbs[word] += 1
+                    print("containing")
+                else:
+                    verbs[word] = 1
+                    print(" not containing")
+
+
+    return (nouns ,verbs)
+
+#nouns,verbs=separating_nouns_and_verbs('4300-0.txt')
+
