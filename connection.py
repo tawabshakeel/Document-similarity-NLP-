3e5a2ce041df62f23a0241e4317a91b504d08821
tawab <tawab2013@gmail.com>
2018-04-09 12:03:39 +0500

    verbs/noun words count with mongodb completed

--- connection.py
+++ connection.py
@@ -1,26 +1,137 @@
-from flask import Flask
-from flask import render_template
-from flask_restful import Resource, Api
-from json import dumps
-from flask.ext.jsonpify import jsonify
-import main as tk
+from flask import Flask,render_template
+from pymongo import MongoClient
+import main
 import json
 
-from pymongo import MongoClient
 client = MongoClient('localhost:27017')
+db = client.connectavo
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('home.html')
+@app.route("/")  # Route 1 Returns the home page
+def home():
+   return render_template("home.html")
 
-@app.route('/hello')
-def check():
-    name='4300-0.txt'
-    data= tk.countingAllWords('4300-0.txt')
-    abc= json.dumps(data)
-    return render_template('table.html', result=data,book=name)
+@app.route("/words_count/<book_id>")
+def word_count(book_id):
+
+    if book_id !='All':
+        query = db.words.find_one({'book_id': book_id})
+        if query:
+            if query["words"]:
+                print("containing ")
+                result = query["words"]
+                result = json.loads(result)
+                return render_template("table.html", result=result, book=book_id)
+            else:
+                data = main.countingAllWords(book_id + ".txt")
+                mongoQuery(book_id,data,1,0,0,0,0)
+                return render_template("table.html", result=data, book=book_id)
+        else:
+            print("empty")
+            try:
+                data =main.countingAllWords(book_id+".txt")
+                mongoQuery(book_id, data, 0, 0,0,0,0)
+                return render_template("table.html", result=data, book=book_id)
+
+            except IOError:
+                print("Error")
+    else:
+        data = main.countingAllWords(book_id + ".txt")
+
+
+
+
+@app.route("/verbs_count/<book_id>")
+def Verbs_Nouns_Count(book_id):
+    if book_id != 'All':
+        query = db.words.find_one({'book_id': book_id})
+        if query:
+            if query["verbs"]:
+                print("in verbs")
+                verbs = json.loads(query["verbs"])
+                nouns = json.loads(query["nouns"])
+                count =json.loads(query["verbs-nouns-count"])
+                return render_template("verbs_nouns.html", nouns=nouns, verbs=verbs,count=count, book=book_id)
+            else:
+                nouns,verbs=main.separating_nouns_and_verbs(book_id + ".txt")
+                count=main.total_verbs_and_nouns(book_id + ".txt")
+                mongoQuery(book_id, 0, 1, 1, nouns,verbs,count)
+                return render_template("verbs_nouns.html", nouns=nouns, verbs=verbs,count=count, book=book_id)
+
+        else:
+            print("no data")
+            nouns,verbs = main.separating_nouns_and_verbs(book_id + ".txt")
+            print(nouns)
+            print("verbs")
+            print(verbs)
+            count = main.total_verbs_and_nouns(book_id + ".txt")
+            mongoQuery(book_id, 0, 0, 1, nouns,verbs,count)
+            return render_template("verbs_nouns.html", nouns=nouns, verbs=verbs,count=count, book=book_id)
+
+
+
+    else:
+        print("All")
+
+
+
+@app.route("/top_10/<book_id>")
+def Similar_Sentences(book_id):
+   return render_template("home.html")
+
+
+
+def mongoQuery(book_id,data,index,type,nouns,verbs,n_v_count):
+    if type == 0:
+        if index == 0 :
+            db.words.insert_one(
+                {
+                    "book_id": book_id,
+                    "words": json.dumps(data),
+                    "verbs": '',
+                    "nouns": '',
+                    "verbs-nouns-count": ''
+
+
+                }).inserted_id
+
+        elif index == 1 :
+            db.words.update(
+                {book_id: book_id},
+                {
+                    "words": json.dumps(data)
+
+                },
+
+            )
+
+
+    elif type == 1:
+        if index == 0:
+            db.words.insert_one(
+                {
+                    "book_id": book_id,
+
+                    "words": '',
+                    "verbs": json.dumps(verbs),
+                    "nouns":json.dumps(nouns),
+                    "verbs-nouns-count" : json.dumps(n_v_count)
+
+                }).inserted_id
+
+        elif index == 1:
+            db.words.update(
+                {book_id: book_id},
+                {
+                    "verbs": json.dumps(verbs),
+                    "nouns":json.dumps(nouns),
+                    "verbs-nouns-count" : json.dumps(n_v_count)
+                },
+
+            )
+
 
 if __name__ == '__main__':
     app.run()
--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import nltk as nt
 def countingAllWords(str):
     counts = dict()
-    for line in open ('books/'+str, 'r', encoding="utf8"):
-      for word in line.split():
-          word = word.lower().replace('.','').replace('?','').replace('!','').replace(':','').replace(',','').replace(')','').replace('(','')
-          if word in counts:
-            counts[word] += 1
-          else:
-            counts[word] = 1
+    try:
+        for line in open ('books/'+str, 'r', encoding="utf-8-sig"):
+          for word in line.split():
+              word = word.lower().replace('.','').replace('?','').replace('!','').replace(':','').replace(',','').replace(')','').replace('(','').replace(';','').replace('"','').replace('*','').replace('[','').replace(']','').replace('_','')
+              if len(word) > 1:
+                if word in counts:
+                    counts[word] += 1
+                else:
+                    counts[word] = 1
+    except:
+        print("sdfdfs")
+        for line in open ('books/'+str, 'r', encoding=None):
+          for word in line.split():
+              word = word.lower().replace('.','').replace('?','').replace('!','').replace(':','').replace(',','').replace(')','').replace('(','').replace(';','').replace('"','').replace('*','').replace('[','').replace(']','').replace('_','')
+
+              if len(word) > 1 :
+                if word in counts:
+                    counts[word] += 1
+                else:
+                    counts[word] = 1
 
     return counts
 
-# print(countingAllWords('4300-0.txt'))
+# print(countingAllWords('3.txt'))
 
 
 
-def separating_nouns_and_verbs(book) :
-    File = open('books/'+book,encoding='utf8')  # open file
+def separating_nouns_and_verbs(book):
+    try:
+        File = open('books/'+book,encoding = "ISO-8859-1")  # open file
+    except:
+        File = open('books/' + book, encoding=None)
     lines = File.read()  # read all lines
     sentences = nt.sent_tokenize(lines)  # tokenize sentences
     nouns = dict()  # empty to array to hold all nouns
@@ -28,24 +44,24 @@ def separating_nouns_and_verbs(book) :
             if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
                 if word in  nouns:
                     nouns[word] += 1
-                    print("containing")
+
                 else:
                     nouns[word] = 1
-                    print(" not containing")
+
             elif (pos == 'VBP' or pos == 'VB' or pos == 'VBZ' ):
                 if word in  verbs:
                     verbs[word] += 1
-                    print("containing")
+
                 else:
                     verbs[word] = 1
-                    print(" not containing")
+
     return (nouns ,verbs)
 
-#nouns,verbs=separating_nouns_and_verbs('4300-0.txt')
+#nouns,verbs=separating_nouns_and_verbs('3.txt')
 
 
 def total_verbs_and_nouns(book):
-    File = open('books/' + book, encoding='utf8')  # open file
+    File = open('books/' + book, encoding = "ISO-8859-1")  # open file
     lines = File.read()  # read all lines
     sentences = nt.sent_tokenize(lines)  # tokenize sentences
     count = dict()  # empty to array to hold all nouns
@@ -63,6 +79,4 @@ def total_verbs_and_nouns(book):
 
     return count
 
-
-# print(total_verbs_and_nouns('4300-0.txt'))
 
