a6c0a2ee60087cdfa0f579ba3215b19d232eeda2
tawab <tawab2013@gmail.com>
2018-04-10 23:42:22 +0500

    Task 1 and 2 with apis/databse completed

--- connection.py
+++ connection.py
@@ -53,8 +53,8 @@ def word_count(book_id):
                     elif document["book_id"] == "3":
                         all_data[document["book_id"]] = json.loads(document["words"])
                else :
-                    data = main.countingAllWords(document["books_id"] + ".txt")
-                    all_data[document["books_id"]]=data
+                    data = main.countingAllWords(document["book_id"] + ".txt")
+                    all_data[document["book_id"]]=data
                     mongoQuery(book_id, data, 1, 0, 0, 0, 0)
 
 
@@ -109,7 +109,7 @@ def word_count(book_id):
                 combined_data["3"] = data
                 mongoQuery("3", data, 0, 0, 0, 0, 0)
             else:
-                if book2["words"] :
+                if book3["words"] :
                     combined_data["3"] = json.loads(book3["words"]);
                 else:
                     data = main.countingAllWords("3" + ".txt")
@@ -136,12 +136,14 @@ def Verbs_Nouns_Count(book_id):
                 count =json.loads(query["verbs-nouns-count"])
                 return render_template("verbs_nouns.html", nouns=nouns, verbs=verbs,count=count, book=book_id)
             else:
+                print("not verbs")
                 nouns,verbs=main.separating_nouns_and_verbs(book_id + ".txt")
                 count=main.total_verbs_and_nouns(book_id + ".txt")
                 mongoQuery(book_id, 0, 1, 1, nouns,verbs,count)
                 return render_template("verbs_nouns.html", nouns=nouns, verbs=verbs,count=count, book=book_id)
 
         else:
+            print("empty record")
             nouns,verbs = main.separating_nouns_and_verbs(book_id + ".txt")
             count = main.total_verbs_and_nouns(book_id + ".txt")
             mongoQuery(book_id, 0, 0, 1, nouns,verbs,count)
@@ -150,61 +152,90 @@ def Verbs_Nouns_Count(book_id):
 
 
     else:
-        print("some data missing")
         book1 = db.words.find_one({'book_id': '1'})
         book2 = db.words.find_one({'book_id': '2'})
         book3 = db.words.find_one({'book_id': '3'})
         print(book1)
         combined_data = dict()
+
+        nested_dict=dict()
         if book1 is None:
-            print("none")
-            data = main.countingAllWords("1" + ".txt")
-            combined_data["1"] = data
-            mongoQuery("1", data, 0, 0, 0, 0, 0)
+            print("book 1 dont exist")
+            nouns, verbs = main.separating_nouns_and_verbs("1" + ".txt")
+            count = main.total_verbs_and_nouns("1" + ".txt")
+            combined_data["1"]=dict((("nouns",nouns),("verbs",verbs),("total_count",count)))
+            mongoQuery("1", 0, 0, 1, nouns, verbs, count)
+
         else:
             print("book1 exist")
-            if book1["words"]:
-                print("book1 words exist")
-                combined_data["1"] = json.loads(book1["words"]);
+            if book1["nouns"]:
+                combined_data["1"] = dict((("nouns", json.loads(book1["nouns"])), ("verbs", json.loads(book1["verbs"])), ("total_count", json.loads(book1["verbs-nouns-count"]))))
+                print(combined_data)
             else:
-                print("book1 words dont exist")
-                data = main.countingAllWords("1" + ".txt")
-                combined_data["1"] = data
-                mongoQuery("1", data, 1, 0, 0, 0, 0)
+
+                nouns, verbs = main.separating_nouns_and_verbs("1" + ".txt")
+                count = main.total_verbs_and_nouns("1" + ".txt")
+                combined_data["1"] = dict((("nouns", nouns), ("verbs", verbs), ("total_count", count)))
+                mongoQuery("1", 0, 1, 1, nouns, verbs, count)
 
         if book2 is None:
             print("book2 dont exist")
-            data = main.countingAllWords("2" + ".txt")
-            combined_data["2"] = data
-            mongoQuery("2", data, 0, 0, 0, 0, 0)
+            nouns, verbs = main.separating_nouns_and_verbs("2" + ".txt")
+            count = main.total_verbs_and_nouns("2" + ".txt")
+            combined_data["2"] = dict((("nouns", nouns), ("verbs", verbs), ("total_count", count)))
+            mongoQuery("2", 0, 0, 1, nouns, verbs, count)
         else:
             print("book2 exist")
-            if book2["words"]:
-                print("book2 words exist")
-                combined_data["2"] = json.loads(book2["words"]);
+            if book2["verbs"]:
+                print("book2 verbs exist")
+                combined_data["2"] = dict((("nouns", json.loads(book2["nouns"])), ("verbs", json.loads(book2["verbs"])),
+                                           ("total_count", json.loads(book2["verbs-nouns-count"]))))
+
             else:
-                print("book2 words dont exist")
-                data = main.countingAllWords("2" + ".txt")
-                combined_data["2"] = data
-                mongoQuery("2", data, 1, 0, 0, 0, 0)
+                nouns, verbs = main.separating_nouns_and_verbs("2" + ".txt")
+                count = main.total_verbs_and_nouns("2" + ".txt")
+                combined_data["2"] = dict((("nouns", nouns), ("verbs", verbs), ("total_count", count)))
+                mongoQuery("2", 0, 1, 1, nouns, verbs, count)
 
         if book3 is None:
-            data = main.countingAllWords("3" + ".txt")
-            combined_data["3"] = data
-            mongoQuery("3", data, 0, 0, 0, 0, 0)
+            print("book 3 dont exist")
+            nouns, verbs = main.separating_nouns_and_verbs("3" + ".txt")
+            count = main.total_verbs_and_nouns("3" + ".txt")
+            combined_data["3"] = dict((("nouns", nouns), ("verbs", verbs), ("total_count", count)))
+            mongoQuery("3", 0, 0, 1, nouns, verbs, count)
         else:
-            if book2["words"]:
-                combined_data["3"] = json.loads(book3["words"]);
+            print("book 3 exist")
+            if book3["verbs"]:
+                print("book 3 verbs exist")
+                combined_data["3"] = dict((("nouns", json.loads(book3["nouns"])), ("verbs", json.loads(book3["verbs"])),
+                                           ("total_count", json.loads(book3["verbs-nouns-count"]))))
             else:
-                data = main.countingAllWords("3" + ".txt")
-                combined_data["3"] = data
-                mongoQuery("3", data, 1, 0, 0, 0, 0)
+                nouns, verbs = main.separating_nouns_and_verbs("3" + ".txt")
+                count = main.total_verbs_and_nouns("3" + ".txt")
+                combined_data["3"] = dict((("nouns", nouns), ("verbs", verbs), ("total_count", count)))
+                mongoQuery("3", 0, 1, 1, nouns, verbs, count)
 
-        A = Counter(combined_data["1"])
-        B = Counter(combined_data["2"])
-        C = Counter(combined_data["3"])
-        r = A + B + C
-        return render_template("all-data.html", result=r)
+        book1_nouns = Counter(combined_data["1"]["nouns"])
+        book1_verbs = Counter(combined_data["1"]["verbs"])
+        book1_total = Counter(combined_data["1"]["total_count"])
+
+        book2_nouns = Counter(combined_data["2"]["nouns"])
+        book2_verbs = Counter(combined_data["2"]["verbs"])
+        book2_total = Counter(combined_data["2"]["total_count"])
+
+        book3_nouns = Counter(combined_data["3"]["nouns"])
+        book3_verbs = Counter(combined_data["3"]["verbs"])
+        book3_total = Counter(combined_data["3"]["total_count"])
+
+        # r = A + B + C
+        nouns_total=book1_nouns+book2_nouns+book3_nouns
+        verbs_total=book1_verbs+book2_verbs+book3_verbs
+        book1_total.pop("document")
+        book2_total.pop("document")
+        book3_total.pop("document")
+        total_sum = book1_total + book2_total + book3_total
+
+        return render_template("all_nouns_verbs.html", nouns=nouns_total,verbs=verbs_total,count=total_sum )
 
 
 @app.route("/top_10/<book_id>")
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import nltk as nt
 from collections import Counter
-def countingAllWords(str):
+import  json
+from collections import OrderedDict
+def countingAllWords(name):
     counts = dict()
     try:
-        for line in open ('books/'+str, 'r', encoding="utf-8-sig"):
+        for line in open ('books/'+name, 'r', encoding="utf-8-sig"):
           for word in line.split():
-              word = word.lower().replace('.','').replace('?','').replace('!','').replace(':','').replace(',','').replace(')','').replace('(','').replace(';','').replace('"','').replace('*','').replace('[','').replace(']','').replace('_','')
+              word = word.lower()
+              word = ''.join(filter(str.isalpha, word))
               if len(word) > 1:
                 if word in counts:
                     counts[word] += 1
@@ -13,10 +16,10 @@ def countingAllWords(str):
                     counts[word] = 1
     except:
 
-        for line in open ('books/'+str, 'r', encoding=None):
+        for line in open ('books/'+name, 'r', encoding=None):
           for word in line.split():
-              word = word.lower().replace('.','').replace('?','').replace('!','').replace(':','').replace(',','').replace(')','').replace('(','').replace(';','').replace('"','').replace('*','').replace('[','').replace(']','').replace('_','')
-
+              word = word.lower()
+              word = ''.join(filter(str.isalpha, word))
               if len(word) > 1 :
                 if word in counts:
                     counts[word] += 1
@@ -42,19 +45,21 @@ def separating_nouns_and_verbs(book):
     for sentence in sentences:
         for word, pos in nt.pos_tag(nt.word_tokenize(str(sentence))):
             word = word.lower()
-            if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
-                if word in  nouns:
-                    nouns[word] += 1
+            word = ''.join(filter(str.isalpha, word))
+            if len(word)> 1:
+                if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
+                    if word in  nouns:
+                        nouns[word] += 1
 
-                else:
-                    nouns[word] = 1
+                    else:
+                        nouns[word] = 1
 
-            elif (pos == 'VBP' or pos == 'VB' or pos == 'VBZ' ):
-                if word in  verbs:
-                    verbs[word] += 1
+                elif (pos == 'VBP' or pos == 'VB' or pos == 'VBZ' ):
+                    if word in  verbs:
+                        verbs[word] += 1
 
-                else:
-                    verbs[word] = 1
+                    else:
+                        verbs[word] = 1
 
     return (nouns ,verbs)
 
@@ -72,10 +77,13 @@ def total_verbs_and_nouns(book):
 
     for sentence in sentences:
         for word, pos in nt.pos_tag(nt.word_tokenize(str(sentence))):
-            if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
-                count["Nouns"] += 1
-            elif(pos == 'VBP' or pos == 'VB' or pos == 'VBZ'):
-                count["Verb"] +=1
+            word = word.lower()
+            word = ''.join(filter(str.isalpha, word))
+            if len(word) > 1:
+                if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS'):
+                    count["Nouns"] += 1
+                elif(pos == 'VBP' or pos == 'VB' or pos == 'VBZ'):
+                    count["Verb"] +=1
 
 
     return count
@@ -97,4 +105,27 @@ def combineDic():
 
 
 
-combineDic()
+def nestedDic():
+    my_dict=dict()
+    new_dic=dict()
+    new_dic["abc"]=1
+    new_dic["def"]=2
+
+    new_dic2 = dict()
+    new_dic2["sabc"] = 1
+    new_dic2["deffsfs"] = 2
+
+    new_dic22 = dict()
+    new_dic22["abc"] = 1
+    new_dic22["def"] = 2
+
+    new_dic33 = dict()
+    new_dic33["sabc"] = 1
+    new_dic33["deffsfs"] = 2
+
+    my_dict["1"]=dict((("words",new_dic),("verbs",new_dic2)))
+    my_dict["2"] = dict((("words", new_dic22), ("verbs", new_dic33)))
+    print(my_dict)
+
+
+#nestedDic()
